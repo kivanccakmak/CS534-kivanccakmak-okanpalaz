df6d82dcc1837c24c6d6b340471790ffcdeb2f14
Kivanc Cakmak <kivanc.cakmak@airties.com>
2016-11-13 05:45:47 -0500

    seperate dummy test and dummy class code

--- src/simulation/My.java
+++ src/simulation/My.java
@@ -1,14 +1,14 @@
 package simulation;
 
 public class My {
-    int val;
+    private int val;
 
     public My() {
         this.val = 0;
     }
 
     public int getVal() {
-        return val;
+        return this.val;
     }
 
     public void setVal(int x) {
--- src/test/Test.java
+++ src/test/Test.java
@@ -1,9 +1,10 @@
-package simulation;
+package test;
+import simulation.*;
 
 public class Test {
     public static void main(String[] args) {
         My x;
         x = new My();
-        System.out.println("val: " + x.val);
+        System.out.println("val: " + x.getVal());
     }
 }
