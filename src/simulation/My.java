757d53e32f5ae6ebe5adc2da3ccf1c489a1fddfb
Kivanc Cakmak <kivanc.cakmak@airties.com>
2016-11-13 05:14:28 -0500

    feat: initial commit on code structuring.

---
+++ src/simulation/My.java
@@ -0,0 +1,17 @@
+package simulation;
+
+public class My {
+    int val;
+
+    public My() {
+        this.val = 0;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int x) {
+        this.val = val;
+    }
+}
---
+++ src/simulation/Test.java
@@ -0,0 +1,9 @@
+package simulation;
+
+public class Test {
+    public static void main(String[] args) {
+        My x;
+        x = new My();
+        System.out.println("val: " + x.val);
+    }
+}
