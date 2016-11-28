5eb90cb5bcbb94dfa9fdbd1279de4768e9ccba01
Kivanc Cakmak <kivanc.cakmak@airties.com>
2016-11-27 20:33:27 -0500

    style: country and day limits removed.
    -since simulationglobals is part of model.

--- src/main/java/SimulationGlobals.java
+++ src/main/java/SimulationGlobals.java
@@ -1,9 +1,6 @@
 public class SimulationGlobals {
-    private static int simDayLimit = 50;
     private static int maxDayToStay = 5;
     private static int minDayToStay = 1;
-    private static int numHorizontalCountries = 3;
-    private static int numVerticalCountries = 3;
 
     private SimulationGlobals() {
     }
@@ -14,17 +11,5 @@ public class SimulationGlobals {
 
     public static int getMinDayToStay() {
         return minDayToStay;
-    }
-
-    public static int getSimDayLimit() {
-        return simDayLimit;
-    }
-
-    public static int getNumHorizontalCountries() {
-        return numHorizontalCountries;
-    }
-
-    public static int getNumVerticalCountries() {
-        return numVerticalCountries;
     }
 }
